a2acd897ee8ff369e5403a36b2782bed925c4c52
Shree Kottes <kottezh@gmail.com>
2024-10-13 15:43:51 +0530

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: ['./templates/**/*.html', './content/**/*.md'],
+    content: ["./templates/**/*.html", "./content/**/*.md"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                halyard: ["HalyardDisplayBook", "sans-serif"],
+                bricolagegrotesque: ["BricolageGrotesque-Bold"],
+                gustavo : ["Gustavo-Bold"],
+            },
+        },
     },
     plugins: [],
-}
-
+};
